bdd7e6ebe0cfc5fcc3f4f0129c2ba3fce8a82c97
Vlad <vladisla1988@gmail.com>
2020-12-24 22:32:17 +0200

    added first part and header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function() {
 
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="arrow"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="arrow"></button>',
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
